02fb1e0f9ecac5224058b4a24630f36aa446e26c
Justin Donaldson <jdonaldson@gmail.com>
2016-10-11 11:00:45 -0700

    [lua] fix missing luv import

--- std/lua/lib/luv/fs/FileSystemPoll.hx
+++ std/lua/lib/luv/fs/FileSystemPoll.hx
@@ -4,7 +4,7 @@ extern class FileSystemPoll {
   static function new_fs_poll() : FileSystemPoll;
   @:native("new_fs_poll") function new() : Void;
 
-  function start(path : String, interval : Int, cb : ResultStatus<Bool>) : Bool;
+  function start(path : String, interval : Int, cb : String->Bool->Void) : Bool;
   function stop() : Bool;
   function getpath() : String;
 }
